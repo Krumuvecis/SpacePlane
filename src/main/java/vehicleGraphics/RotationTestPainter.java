8097784cf4e367bf68aae33b5f8b15551bf7d94c
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-07-06 16:26:11 +0300

    fix

--- src/main/java/vehicleGraphics/RotationTestPainter.java
+++ src/main/java/vehicleGraphics/RotationTestPainter.java
@@ -3,6 +3,7 @@ package vehicleGraphics;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
@@ -15,73 +16,38 @@ class RotationTestPainter implements PainterInterface {
 
     @Override
     public void paint(Graphics g) {
-        int[]
-                location = new int[] {200, 200},
-                size = new int[] {200, 200};
-        //drawRectangles(g, location, size, new int [] {200, 100});
-        drawRotatedCircles(g, location, size);
-    }
-
-    private void drawRectangles(Graphics g, int[] location, int[] size, int[] rectSize) {
-        int[] rectLoc = new int[] {100, 100};
-        String text = "A rotated text";
-        BufferedImage image = new BufferedImage(size[0], size[1], BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = image.createGraphics();
-
-        g2.setColor(Color.cyan);
-        g2.fillRect(rectLoc[0], rectLoc[1], rectSize[0], rectSize[1]);
-        g2.setColor(Color.magenta);
-        g2.drawString(text, rectLoc[0] + rectSize[0] / 2, rectLoc[1] + 20);
-
-        g2.rotate(Math.toRadians(20));
-
-        g2.setColor(Color.magenta);
-        g2.fillRect(rectLoc[0], rectLoc[1], rectSize[0], rectSize[1]);
-        g2.setColor(Color.cyan);
-        g2.drawString("text", rectLoc[0] + rectSize[0] / 2, rectLoc[1] + 20);
-
-        g.drawImage(image, location[0], location[1], imageObserver);
-    }
-
-    private void drawRotatedCircles(Graphics g, int[] location, int[] size) {
-        int maxSize = Math.max(size[0], size[1]);
-        BufferedImage image = new BufferedImage(maxSize * 2, maxSize * 2, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = image.createGraphics();
-
+        int[] location = new int[] {200, 200};
         int radius = 100;
-        int[]
-                circleLoc = new int[] {0, 0},
-                center = new int[] {
-                        circleLoc[0] + radius,
-                        circleLoc[1] + radius};
+        drawRotatedCircles(g, location, radius);
+    }
 
-        //circle 1
-        drawSingleCircle(g2, center, radius, Color.cyan);
+    private void drawRotatedCircles(Graphics g, int[] location, int radius) {
+        BufferedImage image = new BufferedImage(
+                radius * 2,
+                radius * 2,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = image.createGraphics();
+        int[] center = new int[] {radius, radius};
 
-        //circle 2
-        double angle = 30;
-
-        g2.translate(center[0], center[1]);
-        g2.rotate(Math.toRadians(angle));
-        g2.translate(-center[0], -center[1]);
-
-        drawSingleCircle(g2, center, radius, Color.magenta);
-
-        //resets angle transform
-        g2.translate(center[0], center[1]);
-        g2.rotate(Math.toRadians(-angle));
-        g2.translate(-center[0], -center[1]);
-
-        //circle 3
-        angle = 70;
-
-        g2.translate(center[0], center[1]);
-        g2.rotate(Math.toRadians(angle));
-        g2.translate(-center[0], -center[1]);
-
-        drawSingleCircle(g2, center, radius, Color.yellow);
+        drawRotatedCircle(g2, center, radius, 0, Color.cyan);
+        drawRotatedCircle(g2, center, radius, 30, Color.magenta);
+        drawRotatedCircle(g2, center, radius, 70, Color.yellow);
 
         g.drawImage(image, location[0], location[1], imageObserver);
+    }
+
+    private void drawRotatedCircle(Graphics2D g2, int[] center, int radius, double angle, Color color) {
+        g2.transform(getTransform(angle, center));
+        drawSingleCircle(g2, center, radius, color);
+        g2.transform(getTransform(-angle, center)); //resets transform
+    }
+
+    private AffineTransform getTransform(double angle, int[] center) {
+        return new AffineTransform() {{
+            translate(center[0], center[1]);
+            rotate(Math.toRadians(angle));
+            translate(-center[0], -center[1]);
+        }};
     }
 
     private void drawSingleCircle(Graphics2D g2, int[] center, int radius, Color color) {
