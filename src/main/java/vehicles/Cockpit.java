8d40fa5b0779596c65748902f05c97c9d4224275
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-07-22 14:51:16 +0300

    improvements

---
+++ src/main/java/vehicles/Cockpit.java
@@ -0,0 +1,39 @@
+package vehicles;
+
+//
+class Cockpit extends VehiclePart {
+    private static final String PART_NAME = "Cockpit";
+    private static final double
+            MINIMUM_LENGTH = 1.0,       // arbitrary
+            MINIMUM_DIAMETER = 3.0,     // arbitrary
+            VOLUME_PER_PASSENGER = 3.0, // arbitrary
+            PART_DENSITY = 500;         // arbitrary
+
+    //use this for creating new cockpit
+    @SuppressWarnings("SameParameterValue")
+    static Cockpit newCockpit(double targetDiameter, int passengerCount) {
+        double
+                finalDiameter = calculateFinalDiameter(targetDiameter),
+                finalLength = calculateFinalLength(finalDiameter, passengerCount),
+                mass = calculateMass(finalDiameter, finalLength);
+        return new Cockpit(mass, finalLength, finalDiameter);
+    }
+
+    private Cockpit(double mass, double length, double diameter) {
+        super(PART_NAME, mass, length, diameter);
+    }
+
+    private static double calculateFinalDiameter(double targetDiameter) {
+        return Math.max(targetDiameter, MINIMUM_DIAMETER);
+    }
+
+    private static double calculateFinalLength(double diameter, int passengerCount) {
+        return Math.max(
+                CylinderFunctions.getCylinderLength(passengerCount * VOLUME_PER_PASSENGER, diameter),
+                MINIMUM_LENGTH);
+    }
+
+    private static double calculateMass(double diameter, double length) {
+        return PART_DENSITY * CylinderFunctions.getCylinderVolume(diameter, length);
+    }
+}
--- src/main/java/vehicles/SimpleVehicle.java
+++ src/main/java/vehicles/SimpleVehicle.java
@@ -33,56 +33,6 @@ public class SimpleVehicle extends Vehicle {
         CylindricalPart(String name, double length, double diameter) {
             super(name, 0, length, diameter);
         }
-
-        static double getCylinderLength(double volume, double diameter) {
-            return volume / getCylinderCrossSection(diameter);
-        }
-
-        static double getCylinderVolume(double diameter, double length) {
-            return getCylinderCrossSection(diameter) * length;
-        }
-
-        private static double getCylinderCrossSection(double diameter) {
-            return Math.PI * Math.pow(diameter / 2, 2);
-        }
-    }
-
-    //
-    private static class Cockpit extends VehiclePart {
-        private static final String PART_NAME = "Cockpit";
-        private static final double
-                MINIMUM_LENGTH = 1.0,       // arbitrary
-                MINIMUM_DIAMETER = 3.0,     // arbitrary
-                VOLUME_PER_PASSENGER = 3.0, // arbitrary
-                PART_DENSITY = 500;         // arbitrary
-
-        //use this for creating new cockpit
-        @SuppressWarnings("SameParameterValue")
-        static Cockpit newCockpit(double targetDiameter, int passengerCount) {
-            double
-                    finalDiameter = calculateFinalDiameter(targetDiameter),
-                    finalLength = calculateFinalLength(finalDiameter, passengerCount),
-                    mass = calculateMass(finalDiameter, finalLength);
-            return new Cockpit(mass, finalLength, finalDiameter);
-        }
-
-        private Cockpit(double mass, double length, double diameter) {
-            super(PART_NAME, mass, length, diameter);
-        }
-
-        private static double calculateFinalDiameter(double targetDiameter) {
-            return Math.max(targetDiameter, MINIMUM_DIAMETER);
-        }
-
-        private static double calculateFinalLength(double diameter, int passengerCount) {
-            return Math.max(
-                    CylindricalPart.getCylinderLength(passengerCount * VOLUME_PER_PASSENGER, diameter),
-                    MINIMUM_LENGTH);
-        }
-
-        private static double calculateMass(double diameter, double length) {
-            return PART_DENSITY * CylindricalPart.getCylinderVolume(diameter, length);
-        }
     }
 
     //
